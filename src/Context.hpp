62883fc4dce0e80f24735bc74c4cb80dc68750bb
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2013-03-12 23:18:20 +0100

    Starting a Boost Unit Test file and a Visual Studio 2010 project

---
+++ src/Context.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file  Context.hpp
+ * @brief The Context manage all ZeroMQ sockets of an application.
+ *
+ * Copyright (c) 2013 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+#pragma once
+
+
+/**
+ * @brief The Context manage all ZeroMQ sockets of an application.
+ */
+class Context
+{
+public:
+    /// @brief Default constructor
+    Context(void)
+    {
+    }
+private:
+};
---
+++ tests/Boost.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file  Boost.hpp
+ * @brief Boost Unit Tests of this ZMQ C++ wrapper 
+ *
+ * Copyright (c) 2013 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+#define BOOST_TEST_MODULE ZMQCpp
+#include <boost/test/included/unit_test.hpp>
+
+#include "../src/context.hpp"
+
+BOOST_AUTO_TEST_CASE (context_test_case)
+{
+    // Initialize the ZMQ context
+    Context context;
+}
